d5f2f82e8043911cfee235c4cf86fc30a246d180
58866873 <jamesthackett1@gmail.com>
2020-04-20 21:49:29 +0100

    index.js + dependencies

---
+++ index.js
@@ -0,0 +1,35 @@
+var fs = require("fs");
+const Gpio = require('pigpio').Gpio;
+const fan = new Gpio(18, {mode: Gpio.OUTPUT});
+const io = require('@pm2/io')
+
+const Temperature = io.metric({
+  name: 'Temperature',
+  type: 'histogram',
+  measurement: 'mean'
+});
+
+const Speed = io.metric({
+  name: 'Speed',
+  type: 'histogram',
+  measurement: 'mean'
+});
+
+setInterval(() => {
+  let temp = Math.round((fs.readFileSync('../../../../sys/class/thermal/thermal_zone0/temp').toString())/1000)
+  Temperature.set(temp);
+  
+  if (temp < 45) {
+    fan.pwmWrite(50)
+    Speed.set(50)
+  } else if (temp > 70) {
+    fan.pwmWrite(255)
+    Speed.set(255)
+  } else {
+	  let step = 3.2
+	  let speed = Math.round((100) + ((temp - 45) * step))
+    fan.pwmWrite(speed)
+    Speed.set(speed)
+  }
+}, 5000);
+
