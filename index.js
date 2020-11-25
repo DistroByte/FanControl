bf6d25bed87451d0bae077738e8dd1305ff3e8e6
James Hackett <jamesthackett1@gmail.com>
2020-11-25 11:25:36 +0000

    fixes formatting

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var fs = require("fs");
 const Gpio = require('pigpio').Gpio;
-const fan = new Gpio(18, {mode: Gpio.OUTPUT});
+const fan = new Gpio(18, { mode: Gpio.OUTPUT });
 const io = require('@pm2/io')
 
 const Temperature = io.metric({
@@ -16,20 +16,20 @@ const Speed = io.metric({
 });
 
 setInterval(() => {
-  let temp = Math.round((fs.readFileSync('../../../../sys/class/thermal/thermal_zone0/temp').toString())/1000)
+  let temp = Math.round((fs.readFileSync('../../../../sys/class/thermal/thermal_zone0/temp').toString()) / 1000)
   Temperature.set(temp);
-  
+
   if (temp < 45) {
     fan.pwmWrite(50)
-    Speed.set(100)
+    Speed.set((50 / 255) * 100)
   } else if (temp > 70) {
     fan.pwmWrite(255)
-    Speed.set(255)
+    Speed.set((255 / 255) * 100)
   } else {
-	  let step = 3.2
-	  let speed = Math.round((100) + ((temp - 45) * step))
+    let step = 3.2
+    let speed = Math.round((100) + ((temp - 45) * step))
     fan.pwmWrite(speed)
-    Speed.set(speed)
+    Speed.set((speed / 255) * 100)
   }
-}, 5000);
+}, 10000);
 
